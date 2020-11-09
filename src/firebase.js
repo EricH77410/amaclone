60905a87a1c5a795890f87053dc7f0c2183675cf
Eric Hamimi <eric.hamimi@csdental.com>
2020-11-09 15:28:26 +0100

    start app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWDsulmMrn8OXy2MW-hWAQHDF-uHDaGiw',
+  authDomain: 'amaclone-84887.firebaseapp.com',
+  databaseURL: 'https://amaclone-84887.firebaseio.com',
+  projectId: 'amaclone-84887',
+  storageBucket: 'amaclone-84887.appspot.com',
+  messagingSenderId: '534789994313',
+  appId: '1:534789994313:web:c68fa67071bf5cf2104c98',
+}
