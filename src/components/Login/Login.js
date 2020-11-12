835c69cbe0bbeaf320e5d95c0b6a32c873beec00
Eric Hamimi <eric.hamimi@csdental.com>
2020-11-12 15:17:36 +0100

    add firebase auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from '../../firebase'
 
 import './Login.css'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const onLoginSubmit = (e) => {
     e.preventDefault()
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => console.log(auth))
+      .catch((err) => console.warn(err))
+  }
+
+  const register = (e) => {
+    e.preventDefault()
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => console.log(auth))
+      .catch((err) => console.warn(err))
   }
 
   return (
-    <div className='login'>
-      <Link to='/'>
+    <div className="login">
+      <Link to="/">
         <img
-          className='login__logo'
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
-          alt='Logo'
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt="Logo"
         />
       </Link>
-      <div className='login__container'>
+      <div className="login__container">
         <h1>Sign in</h1>
         <form onSubmit={onLoginSubmit}>
           <h5>Email</h5>
-          <input type='text' />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type='password' />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className='login__signinButton'>Sign In</button>
+          <button className="login__signinButton">Sign In</button>
         </form>
 
         <p>
@@ -35,7 +60,7 @@ const Login = () => {
           Notice.
         </p>
 
-        <button className='login__registerButton'>
+        <button className="login__registerButton" onClick={register}>
           Create your Amaclone Account
         </button>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: 'AIzaSyCWDsulmMrn8OXy2MW-hWAQHDF-uHDaGiw',
   authDomain: 'amaclone-84887.firebaseapp.com',
@@ -7,3 +9,10 @@ const firebaseConfig = {
   messagingSenderId: '534789994313',
   appId: '1:534789994313:web:c68fa67071bf5cf2104c98',
 }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
